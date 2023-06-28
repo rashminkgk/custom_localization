6b23e752adfbdf3b4e0a61cb64701af7c5a1a150
Rashmin <rashmin.dungrani@kgkmail.com>
2023-06-28 19:19:29 +0530

    feat: ✨ Change path

--- custom_localization_generator/lib/builder.dart
+++
@@ -1,8 +0,0 @@
-import 'package:build/build.dart';
-import 'package:custom_localization_generator/custom_localization_generator.dart';
-import 'package:source_gen/source_gen.dart';
-
-Builder customLocalizationGenerator(BuilderOptions options) {
-  return SharedPartBuilder(
-      [CustomLocalizationGenerator()], 'custom_localization');
-}
